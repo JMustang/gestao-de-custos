6cc57f13f02be27f920ace39842f0dd5294c013b
Junior <eecfredes@gmail.com>
2025-09-14 19:53:21 -0300

    :tada:Startando o projeto

---
+++ src/main/java/br/com/javatutor/gestao_de_custos/GestaoDeCustosPessoaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.javatutor.gestao_de_custos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeCustosPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeCustosPessoaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javatutor/gestao_de_custos/GestaoDeCustosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javatutor.gestao_de_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeCustosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
