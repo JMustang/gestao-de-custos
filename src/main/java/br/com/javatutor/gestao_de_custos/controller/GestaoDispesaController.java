1d6c51ca9c22b1a80d273c808409b22b63b54e64
Junior <eecfredes@gmail.com>
2025-09-16 18:27:22 -0300

    :rocket:Gestão controller

---
+++ src/main/java/br/com/javatutor/gestao_de_custos/controller/GestaoDispesaController.java
@@ -0,0 +1,13 @@
+package br.com.javatutor.gestao_de_custos.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/gestao")
+public class GestaoDispesaController {
+
+    @PostMapping("/create")
+    public void create() {
+
+    }
+}
