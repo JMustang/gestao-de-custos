cf3b422f584702a7cad5b1eb815a42906f30fad0
Junior <eecfredes@gmail.com>
2025-09-18 21:34:55 -0300

    :rocket:Cadastros despesas use case

---
+++ src/main/java/br/com/javatutor/gestao_de_custos/useCases/CadastroDespesaUseCase.java
@@ -0,0 +1,10 @@
+package br.com.javatutor.gestao_de_custos.useCases;
+
+import br.com.javatutor.gestao_de_custos.entity.Despesa;
+
+public class CadastroDespesaUseCase {
+
+    public void execute(Despesa despesa) {
+        System.out.println(despesa);
+    }
+}
