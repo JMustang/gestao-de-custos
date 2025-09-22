668faeb84f625061727ba38ddee5a749ac8d18a7
Junior <eecfredes@gmail.com>
2025-09-21 21:35:36 -0300

    :rocket:Usando service

--- src/main/java/br/com/javatutor/gestao_de_custos/useCases/CadastroDespesaUseCase.java
+++ src/main/java/br/com/javatutor/gestao_de_custos/useCases/CadastroDespesaUseCase.java
@@ -1,12 +1,13 @@
 package br.com.javatutor.gestao_de_custos.useCases;
 
+import org.springframework.stereotype.Service;
+
 import br.com.javatutor.gestao_de_custos.entity.Despesa;
 
+@Service
 public class CadastroDespesaUseCase {
 
     public void execute(Despesa despesa) {
-        System.out.println("Categoria " + despesa.getCategoria());
-        System.out.println("Email " + despesa.getEmail());
         System.out.println(despesa);
     }
 }
