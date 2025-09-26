ee26d2b24be98ece05d5c28ba1e86c47874a713c
Junior <eecfredes@gmail.com>
2025-09-25 23:13:17 -0300

    :rocket: Adicionando Repository no user case

--- src/main/java/br/com/javatutor/gestao_de_custos/useCases/CadastroDespesaUseCase.java
+++ src/main/java/br/com/javatutor/gestao_de_custos/useCases/CadastroDespesaUseCase.java
@@ -1,13 +1,18 @@
 package br.com.javatutor.gestao_de_custos.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.javatutor.gestao_de_custos.entity.Despesa;
+import br.com.javatutor.gestao_de_custos.repository.DespesaRepository;
 
 @Service
 public class CadastroDespesaUseCase {
 
+    @Autowired
+    private DespesaRepository despesaRepository;
+
     public void execute(Despesa despesa) {
-        System.out.println(despesa);
+        despesaRepository.save(despesa);
     }
 }
