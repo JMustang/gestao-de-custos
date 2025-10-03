37a20277172cdbfdf7e90543fcedb0c5c0563049
Junior <eecfredes@gmail.com>
2025-10-02 22:46:49 -0300

    :rocket: Classe Gestão de despesa seeder

--- src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDeDespesaSeeder.java
+++ src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDeDespesaSeeder.java
@@ -1,14 +1,39 @@
 package br.com.javatutor.gestao_de_custos.performance;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import br.com.javatutor.gestao_de_custos.entity.Despesa;
+import br.com.javatutor.gestao_de_custos.repository.DespesaRepository;
 
 @Component
 public class GestaoDeDespesaSeeder implements CommandLineRunner {
 
+    @Autowired
+    DespesaRepository despesaRepository;
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Rodando junto com a aplicação");
+        List<Despesa> despesas = new ArrayList<>();
+
+        for (int i = 0; i <= 150000; i++) {
+            Despesa despesa = new Despesa();
+            despesa.setDescricao("Gasto n*: " + i);
+            despesa.setValor(BigDecimal.valueOf(10 + (i % 50)));
+            despesa.setData(LocalDate.now().minusDays(i % 30));
+            despesa.setCategoria("TESTE");
+            despesa.setEmail("performance@gmail.com");
+
+            despesas.add(despesa);
+        }
+
+        despesaRepository.saveAll(despesas);
     }
 
 }
