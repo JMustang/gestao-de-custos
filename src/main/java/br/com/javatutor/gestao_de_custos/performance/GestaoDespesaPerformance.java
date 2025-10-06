4a3a0ccead58259b016571cc67fd3bc2ec80a3bd
Junior <eecfredes@gmail.com>
2025-10-06 20:53:49 -0300

    :rocket:Rest controller

--- src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
+++ src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.javatutor.gestao_de_custos.entity.Despesa;
 import br.com.javatutor.gestao_de_custos.repository.DespesaRepository;
 
 @RequestMapping("/gestao/performance")
+@RestController
 public class GestaoDespesaPerformance {
 
     @Autowired
