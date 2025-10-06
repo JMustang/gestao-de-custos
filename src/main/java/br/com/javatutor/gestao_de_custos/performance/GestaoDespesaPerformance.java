f951a4cdbdb47498032946d17846e4d5402e92b6
Junior <eecfredes@gmail.com>
2025-10-05 22:52:23 -0300

    :rocket: Testando o tempo de execução sem paginação

--- src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
+++ src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.javatutor.gestao_de_custos.entity.Despesa;
@@ -15,9 +16,13 @@ public class GestaoDespesaPerformance {
     @Autowired
     DespesaRepository repository;
 
+    @GetMapping("/sem-paginacao")
     public ResponseEntity<List<Despesa>> listarSemPaginacao() {
+        long inicio = System.currentTimeMillis();
         var despesas = repository.findAll();
 
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo Gasto sem paginação: " + (fim - inicio) + " ms");
         return ResponseEntity.ok(despesas);
     }
 }
