c05fcc4c3d03b277e7cc47598821b4b056fc350b
Junior <eecfredes@gmail.com>
2025-10-07 22:22:23 -0300

    :rocket: paginação usando pageable

--- src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
+++ src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
@@ -3,7 +3,10 @@ package br.com.javatutor.gestao_de_custos.performance;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,17 @@ public class GestaoDespesaPerformance {
 
         long fim = System.currentTimeMillis();
         System.out.println("Tempo Gasto sem paginação: " + (fim - inicio) + " ms");
+        return ResponseEntity.ok(despesas);
+    }
+
+    @GetMapping("/com-paginacao")
+    public ResponseEntity<Page<Despesa>> listarComPaginacao(Pageable pageable) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        var despesas = repository.findAll(pageable);
+
+        stopWatch.stop();
+        System.out.println("Tempo Gasto com paginação: " + stopWatch.getTotalTimeMillis() + " ms");
         return ResponseEntity.ok(despesas);
     }
 }
