1b292900aa0c7c2f299b6871546ae69b81bb875b
Junior <eecfredes@gmail.com>
2025-10-08 22:39:20 -0300

    :rocket:Fazendo busca por email

--- src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
+++ src/main/java/br/com/javatutor/gestao_de_custos/performance/GestaoDespesaPerformance.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +37,17 @@ public class GestaoDespesaPerformance {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         var despesas = repository.findAll(pageable);
+
+        stopWatch.stop();
+        System.out.println("Tempo Gasto com paginação: " + stopWatch.getTotalTimeMillis() + " ms");
+        return ResponseEntity.ok(despesas);
+    }
+
+    @GetMapping("/com-paginacao/{email}")
+    public ResponseEntity<Page<Despesa>> listarComPaginacao(@PathVariable String email, Pageable pageable) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        var despesas = repository.findByEmail(email, pageable);
 
         stopWatch.stop();
         System.out.println("Tempo Gasto com paginação: " + stopWatch.getTotalTimeMillis() + " ms");
